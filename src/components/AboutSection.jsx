b6b35cd3eb2dce3371ddcdfb504c512715d38d40
mukthar <mukthar2004@gmail.com>
2026-02-01 13:06:42 +0530

    desktop optimization

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -7,11 +7,17 @@ function AboutSection() {
 
     return (
         <section className="about-section">
+            <div className="about-glow-top-right">
+                <div className="about-glow-ellipse"></div>
+            </div>
+
             <div className="about-content">
                 <div className="about-left">
                     <h2 className="about-heading">
-                        We think. We build.<br />We stay.
+                        We think. We build.<br />
+                        We stay.
                     </h2>
+
                     <div className="about-text-container">
                         <p className="about-description">
                             We make AI adoption real.<br /><br />
@@ -20,10 +26,12 @@ function AboutSection() {
                         </p>
                         <Link to="/whatwedo" className="btn-see-how">SEE HOW WE WORK</Link>
                     </div>
+
                     <div className="about-image-abstract">
                         <img src={imgAbstract} alt="" className="abstract-image" />
                     </div>
                 </div>
+
                 <div className="about-right">
                     <div className="about-image-container">
                         <img src={imgWomanWindow} alt="" className="about-image-main" />
